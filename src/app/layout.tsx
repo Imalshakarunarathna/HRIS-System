22a8e95ee698fe628ca62ce515300c732ed10e50
Imalsha-karunarathna <laksarani@liveroom.dev>
2025-08-20 16:37:20 +0530

    change layout description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Enterprise HRIS",
-  description: "Human Resources Information System",
+  description: "Human Resources Information System-Liveroom Technologies",
 };
 
 export default function RootLayout({
